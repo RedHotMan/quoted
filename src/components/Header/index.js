8ee88b7d3e7088e2c742ca8dfddc2b904e03b7ca
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-11-13 00:05:14 +0100

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HeaderWrapper, Title, Subtitle } from './style';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Title>Quoted</Title>
+      <Subtitle>List quotes from a tweet</Subtitle>
+    </HeaderWrapper>
+  )
+}
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  margin: 0 0 60px 0;
+`;
+
+export const Title = styled.h1`
+  font-weight: 900;
+  font-size: 3em;
+  margin: 0 0 5px 0;
+`;
+
+export const Subtitle = styled.h2`
+  margin: 0;
+`;
