656fbe828ccffe94daecb57a03a13e71f0bb2b53
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-12-01 13:57:16 +0100

    Setup proxy for the client and install dotenv to get acces to .env in production mode

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config({ path: '.env' });
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const Twitter = require('./twitter').Twitter;
@@ -6,6 +8,14 @@ const Twitter = require('./twitter').Twitter;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve('build')));
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.resolve('build', 'index.html'));
+  });
+}
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
@@ -25,4 +35,4 @@ app.get('/api/getQuotesFromTweet/:userId/:tweetId', (req, res) => {
   );
 });
 
-app.listen(3001, () => console.log('Express server is running on localhost:3001'));
+app.listen(3003, () => console.log('Express server is running on localhost:3003'));
