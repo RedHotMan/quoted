8b8c39d8393e323e0c333fdc9d7d19f12976425b
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-12-01 17:06:23 +0100

    Add github corner with quoted repo link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Layout from './containers/Layout';
 import { Provider } from 'react-redux';
 import store from './store';
+import GithubCorner from 'react-github-corners';
+import 'react-github-corners/dist/GithubCorner.css';
 
 function App() {
   return (
     <Provider store={store}>
+      <GithubCorner url="https://github.com/RedHotMan/quoted" backgroundColor="#255FFF" target="_blank"/>
       <Layout />
     </Provider>
   );
