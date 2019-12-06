eb480bd898ce388e55a75c84896cfdd445f23d6e
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-12-06 01:08:26 +0100

    Simplify url tweet check util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,6 @@
 export const isTweetUrl = (url) => {
-  let validUrl = true;
   const expression = new RegExp(/^(?:http(s)?:\/\/)?twitter.com\/[\w.-]*\/status\/[0-9]*/gm);
-
-  try {
-    Boolean(new URL(url))
-  } catch (e) {
-    validUrl = false;
-  }
-
-  return expression.test(url) || validUrl;
+  return expression.test(url);
 }
 
 export const getUserIdAndTweetIdFromUrl = url => {
